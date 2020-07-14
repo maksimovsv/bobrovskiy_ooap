24edca0fe9cb6a1d53e97c5888e31b187ee07e53
Sergey Maksimov <dowsurfer@gmail.com>
2020-07-14 20:34:17 +0300

    added DynArray

---
+++ DynArray/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DynArray
+{
+    public abstract class DynArray<T>
+    {
+        // конструктор
+        public DynArray(){}
+
+        // КОМАНДЫ
+
+        // постусловие: создан новый массив;
+        public abstract void make_array(int capacity);
+
+        // постусловие: в конец массива добавлен новый элемент;
+        public abstract void append(T item);
+
+        // предусловие: индекс находится в границах массива
+        // постусловие:  в i-ю позицию массива добавлен новый элемент, все последующие элементы сдвинуты вперед;
+        public abstract void insert(T item, int i);
+
+        // предусловие: массив не пустой; индекс находится в границах массива
+        // постусловие:  из i-й позиции массива удалён элемент, все последующие элементы сдвинуты назад;
+        public abstract void remove(int i);
+        
+        // постусловие:  удалены все элементы массива
+        public abstract void clear();
+
+        // ЗАПРОСЫ
+
+        // предусловие: массив не пустой; индекс находится в границах массива
+        public abstract T get_item(int i);
+
+        public abstract int size();
+
+        // запросы статусов
+        public abstract int get_insert_status();
+        public abstract int get_remove_status();
+        public abstract int get_get_status();
+
+
+
+    }
+}

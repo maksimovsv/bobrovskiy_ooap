096ac38f2716bf02efb320b42eb4374adfc90ad7
Sergey Maksimov <dowsurfer@gmail.com>
2020-07-15 18:01:09 +0300

    added BloomFilter

---
+++ OOP1/BloomFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP1
+{
+    public abstract class BloomFilter
+    {
+        protected BloomFilter(){}
+
+        // КОМАНДЫ
+
+        // постусловие: добавлена строка str1 в фильтр
+        public abstract void add(string str1);
+        
+        // ЗАПРОСЫ
+        public abstract bool is_value(string str1); // проверка, имеется ли строка str1 в фильтре
+    }
+}

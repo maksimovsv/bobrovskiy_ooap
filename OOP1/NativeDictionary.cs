f53a2acfa756b079b312ce0b8d553e289c89c665
Sergey Maksimov <dowsurfer@gmail.com>
2020-07-15 16:10:55 +0300

    added NativeDictionary

---
+++ OOP1/NativeDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public abstract class NativeDictionary<T>
+    {
+        // конструктор
+        // постусловие: создана пустой словарь заданного размера
+        protected NativeDictionary(int size){}
+
+        // команды
+
+        // предусловие: в словаре есть место для добавления
+        // постусловие: в словарь добавлено новое значение
+        public abstract void put(string key, T value);
+
+        // предусловие: в словаре имеется ключ key;
+        // постусловие: из словаря удалено значение с ключом key
+        public abstract void remove(string key);
+
+        // запросы
+
+        public abstract bool is_key(string key); // содержится ли ключ value в словаре
+
+        // предусловие: в словаре имеется ключ key;
+        public abstract bool get(string key); // получить значение по ключу
+
+        public abstract int size(); // количество элементов в словаре
+
+        // запросы статусов (возможные значения статусов)
+        public abstract int get_put_status(); // успешно; 
+        // система коллизий не смогла найти свободный слот для пары ключ-значение
+
+        public abstract int get_remove_status(); // успешно; ключа нет в словаре
+
+        public abstract int get_get_status(); // успешно; ключа нет в словаре
+
+    }
+}

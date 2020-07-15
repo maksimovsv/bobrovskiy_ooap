9ae38528ba4b3df3573761b7e18a6f880ade39da
Sergey Maksimov <dowsurfer@gmail.com>
2020-07-15 14:16:37 +0300

    added HashTable

---
+++ HashTable/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HashTable
+{
+    public abstract class HashTable<T>
+    {
+        protected HashTable(int size){}
+
+        //КОМАНДЫ
+
+        // постусловие: добавлен новый элемент
+        public abstract void put(T Value);
+
+        // постусловие: значение удалено
+        public abstract void delete(T Value);
+
+        //ЗАПРОСЫ
+
+        public abstract bool get_put_status();
+        public abstract bool get_delete_status();
+
+    }
+}

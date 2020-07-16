b02770d9bfa871b5bab4a271e42b9fd52b1d6d65
Sergey Maksimov <dowsurfer@gmail.com>
2020-07-16 16:55:55 +0300

    added task01

---
+++ OOP2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void TestTask01()
+        {
+            Animal cow = new Animal("cow");
+            Console.WriteLine(cow.Sound());
+
+            Dog dog = new Dog("dog");
+            Console.WriteLine(dog.Sound());
+
+            var lady = new LadyWithAnAnimal(dog);
+            Console.WriteLine(lady.Sound());
+
+            Animal[] zoo = {cow, dog};
+            foreach (var animal in zoo)
+            {
+                Console.WriteLine("animal in zoo says " + animal.Sound());
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestTask01();
+        }
+    }
+}
---
+++ OOP2/Task01.cs
@@ -0,0 +1,67 @@
+﻿namespace OOP2
+{
+    // ЗАДАНИЕ 1
+    class Animal
+    {
+        protected string name;
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual string Sound()
+        {
+            return $"Animal {name} sound";
+        }
+    }
+
+    class Dog : Animal //наследование
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override string Sound() // полиморфизм
+        {
+            return $"Animal {name} sound";
+        }
+    }
+
+    class LadyWithAnAnimal
+    {
+        private Animal animal; // композиция
+        public LadyWithAnAnimal(Animal animal)
+        {
+            this.animal = animal;
+        }
+
+        public string Sound()
+        {
+            return "Lady's " + animal.Sound();
+        }
+    }
+
+    // ЗАДАНИЕ 2
+
+    class Transport
+    {
+        public virtual void Move(){}
+
+        public virtual void Stop(){}
+    }
+
+    class Bus : Transport // расширение - добавляем новые методы
+    {
+        public void LoadPassengers(){}
+    }
+
+    class Vehicle : Transport
+    {
+        public override void Move(){} // сужение - переопределяем метод родительского класса
+    }
+
+    // ЗАДАНИЕ 3
+    /* В C# концепция класс-как-модуль, поддерживается, например, с помощью библиотек .dll. Можно скомпилировать даже один класс как библиотеку и в дальнейшем
+     * подключать ее в нужных проектах
+     */
+}
